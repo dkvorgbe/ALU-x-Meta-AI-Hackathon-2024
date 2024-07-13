ef2a6848cff1f5ed058068574aa2b10fa80e75f7
Delali Vorgbe <dvorgbe@gmail.com>
2024-07-13 22:35:51 +0000

    Resolved issue of no model node between prompt and output

--- virtualenv-example/langflow/chat_with_context.py
+++ virtualenv-example/langflow/chat_with_context.py
@@ -9,7 +9,7 @@ TWEAKS = {
   },
   "ChatInput-THQC8": {
     "files": "",
-    "input_value": "How many hours can a hackathon last?",
+    "input_value": "What programs does the university offer?",
     "sender": "User",
     "sender_name": "User",
     "session_id": "",
@@ -45,15 +45,43 @@ TWEAKS = {
     "sender_name": "AI",
     "session_id": "",
     "store_message": True
+  },
+  "OllamaModel-F5mAo": {
+    "base_url": "http://localhost:11434",
+    "format": "",
+    "input_value": "",
+    "metadata": {},
+    "mirostat": "Disabled",
+    "mirostat_eta": None,
+    "mirostat_tau": None,
+    "model": "llama3:latest",
+    "num_ctx": None,
+    "num_gpu": None,
+    "num_thread": None,
+    "repeat_last_n": None,
+    "repeat_penalty": None,
+    "stop_tokens": "",
+    "stream": False,
+    "system": "",
+    "system_message": "",
+    "tags": "",
+    "temperature": 0.2,
+    "template": "",
+    "tfs_z": None,
+    "timeout": None,
+    "top_k": None,
+    "top_p": None,
+    "verbose": False
   }
 }
 
 flow_path = os.path.join(os.path.dirname(__file__), "flows/chat_with_vector_response_as_context.json")
 
 result = run_flow_from_json(flow=flow_path,
-                            input_value="how long should a hackathon be run?",
+                            input_value="message",
                             fallback_to_env_vars=True, # False by default
                             tweaks=TWEAKS)
 
-from pprint import pprint
-pprint(result)
+llm_response = result[0].outputs[0].results
+
+print(llm_response)
